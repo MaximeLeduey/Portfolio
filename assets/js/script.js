0bc73ddff8952063337efb2977a9edf77851e50c
MaximeLeduey <91879887+MaximeLeduey@users.noreply.github.com>
2022-12-27 22:04:05 +0100

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,86 @@
 
 // animation au scroll des projets 
 
-const slider = document.querySelector('.projects_slider');
-let activeItemNbr = 1;
+// const slider = document.querySelector('.projects_slider');
+// let activeItemNbr = 1;
 
 
-slider.addEventListener('scroll', () => {
-    if(slider.scrollLeft < 250) {
-        activeItemNbr = 1;
+// slider.addEventListener('scroll', () => {
+//     if(slider.scrollLeft < 250) {
+//         activeItemNbr = 1;
+//     }
+//     else if(slider.scrollLeft < 500) {
+//         activeItemNbr = 2;
+//     }
+//     else {
+//         activeItemNbr = 3;
+//         slider.removeEventListener('scroll', () => {
+//             console.log('remove');
+//         }, true)
+//     }
+//     const items = document.querySelectorAll('.projects_item');
+//     items.forEach(item => {
+//         if(item.classList.contains(`item-${activeItemNbr}`)) {
+//             item.classList.add('active');
+//         }
+//         else {
+//             item.classList.remove('active');
+//         }
+//     })
+//     console.log(slider.scrollLeft);
+// }, true)
+
+const slider = document.querySelector('.projects_slider'),
+    slides = document.querySelectorAll('.projects_item'),
+    slideSize = 500;
+
+
+let clicPosition = 0,
+    dragPosition = 0,
+    startPosition = 0,
+    endPosition = 0,
+    limit = 70;
+
+slider.addEventListener('pointerdown', dragStart);
+
+function dragStart(e) {
+
+    e.preventDefault();
+
+    slider.style.left = slider.startPosition;
+
+    clicPosition = e.clientX;
+
+    document.addEventListener('pointermove', dragging);
+    document.addEventListener('pointerup', dragEnd);
+
+}
+
+function dragging(e) {
+
+    e.preventDefault();
+    
+    dragPosition = clicPosition - e.clientX;
+
+    console.log(dragPosition);
+    clicPosition = e.clientX;
+
+    if(slider.offsetLeft - dragPosition > 0) {
+        console.log('stop');
+        return;
     }
-    else if(slider.scrollLeft < 500) {
-        activeItemNbr = 2;
-    }
-    else {
-        activeItemNbr = 3;
-        slider.removeEventListener('scroll', () => {
-            console.log('remove');
-        }, true)
-    }
-    const items = document.querySelectorAll('.projects_item');
-    items.forEach(item => {
-        if(item.classList.contains(`item-${activeItemNbr}`)) {
-            item.classList.add('active');
-        }
-        else {
-            item.classList.remove('active');
-        }
-    })
-    console.log(slider.scrollLeft);
-}, true)
 
+    slider.style.left = (slider.offsetLeft - dragPosition) + "px";
 
+    console.log(slider.offsetLeft);
+}
+
+function dragEnd(e) {
+
+    e.preventDefault();
+
+    console.log('fini')
+}
 
 
 // gestion du burger menu
